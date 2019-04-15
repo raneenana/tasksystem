fb410afacd57489251f69ba3825bcfa1a0f7b171
zhangna <1092433129@qq.com>
2019-04-15 08:29:40 +0800

    班级排版

--- vue-element-admin/src/views/classes/class.vue
+++ vue-element-admin/src/views/classes/class.vue
@@ -1,23 +1,114 @@
 <template>
-  <div class="chart-container">
-    <chart height="100%" width="100%" />
+  <div class="layout" style="padding: 0px 24px 24px;">
+    <h2 style="padding: 20px 0px; margin-top: 10px;">班级管理</h2>
+    <div class="layout-content">
+      <el-button type="primary" class="button">+添加班级</el-button>
+      <el-table
+        :data="tableData"
+        style="width: 100%"
+      ><el-table-column
+         prop="date"
+         label="班级名"
+         width="312"
+       />
+        <el-table-column
+          prop="name"
+          label="课程名"
+          width="594"
+        />
+        <el-table-column
+          prop="address"
+          label="教师号"
+          width="312"
+        />
+        <el-table-column
+          prop="edit"
+          label="操作"
+          width="312"
+        >
+          <span>修改</span>|<span>删除</span>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
 <script>
-import Chart from '@/components/Charts/keyboard'
-
 export default {
-  name: 'KeyboardChart',
-  components: { Chart }
+  data() {
+    return {
+      tableData: [{
+        date: '1701B',
+        name: 'node基础',
+        address: '34303',
+        edit: ''
+      }, {
+        date: '1701B',
+        name: 'node基础',
+        address: '34303',
+        edit: ''
+      }, {
+        date: '1701B',
+        name: 'node基础',
+        address: '34303',
+        edit: ''
+      }, {
+        date: '1701B',
+        name: 'node基础',
+        address: '34303',
+        edit: ''
+      }]
+    }
+  }
 }
 </script>
 
 <style scoped>
-.chart-container{
-  position: relative;
-  width: 100%;
-  height: calc(100vh - 84px);
-}
+  .chart-container {
+    position: relative;
+    width: 100%;
+    height: calc(100vh - 84px);
+  }
+  .layout {
+    display: flex;
+    flex-direction: column;
+    flex: auto;
+    background: #f0f2f5;
+    min-height: 0;
+  }
+  thead{
+    color: rgba(0, 0, 0, 0.85)!important;
+    background: #f0f2f5!important;
+    font-weight: 100!important;
+  }
+  h2{
+      margin-top: 0;
+      margin-bottom: 0.5em;
+      color: rgba(0, 0, 0, 0.85);
+      font-weight: 500;
+  }
+  .layout-content {
+      flex: auto;
+      min-height: 0;
+      box-sizing: border-box;
+  }
+  .layout-content {
+      flex: auto;
+      min-height: 0;
+      background: rgb(255, 255, 255);
+      padding: 24px;
+      margin: 0px 0px 20px;
+      border-radius: 10px;
+  }
+  .button{
+    display: flex;
+    background: #0139fd;
+    margin-bottom: 15px;
+  }
+  .table{
+    width: 100%;
+    padding: 0 10px;
+    box-sizing: border-box;
+  }
 </style>
 
--- vue-element-admin/src/views/classes/classroom.vue
+++ vue-element-admin/src/views/classes/classroom.vue
@@ -1,23 +1,95 @@
 <template>
-  <div class="chart-container">
-    <chart height="100%" width="100%" />
+  <div class="layout" style="padding: 0px 24px 24px;">
+    <h2 style="padding: 20px 0px; margin-top: 10px;">教室管理</h2>
+    <div class="layout-content">
+      <el-button type="primary" class="button">+添加教室</el-button>
+      <el-table
+        :data="tableData"
+        style="width: 100%"
+      ><el-table-column
+         prop="date"
+         label="教室号"
+         width="887"
+       />
+        <el-table-column
+          prop="edit"
+          label="操作"
+        >
+          <span>修改</span>|<span>删除</span>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
 <script>
-import Chart from '@/components/Charts/keyboard'
-
 export default {
-  name: 'KeyboardChart',
-  components: { Chart }
+  data() {
+    return {
+      tableData: [{
+        date: '1701B',
+        edit: ''
+      }, {
+        date: '1701B',
+        edit: ''
+      }, {
+        date: '1701B',
+        edit: ''
+      }, {
+        date: '1701B',
+        edit: ''
+      }]
+    }
+  }
 }
 </script>
 
 <style scoped>
-.chart-container{
-  position: relative;
-  width: 100%;
-  height: calc(100vh - 84px);
-}
+  .chart-container {
+    position: relative;
+    width: 100%;
+    height: calc(100vh - 84px);
+  }
+  .layout {
+    display: flex;
+    flex-direction: column;
+    flex: auto;
+    background: #f0f2f5;
+    min-height: 0;
+  }
+  thead{
+    color: rgba(0, 0, 0, 0.85)!important;
+    background: #f0f2f5!important;
+    font-weight: 100!important;
+  }
+  h2{
+      margin-top: 0;
+      margin-bottom: 0.5em;
+      color: rgba(0, 0, 0, 0.85);
+      font-weight: 500;
+  }
+  .layout-content {
+      flex: auto;
+      min-height: 0;
+      box-sizing: border-box;
+  }
+  .layout-content {
+      flex: auto;
+      min-height: 0;
+      background: rgb(255, 255, 255);
+      padding: 24px;
+      margin: 0px 0px 20px;
+      border-radius: 10px;
+  }
+  .button{
+    display: flex;
+    background: #0139fd;
+    margin-bottom: 15px;
+  }
+  .table{
+    width: 100%;
+    padding: 0 10px;
+    box-sizing: border-box;
+  }
 </style>
 
