bcf66d11e8a2eae2e94e60de25ecd914c36dbbe4
raneenana <17773439@qq.com>
2019-04-23 08:02:51 +0800

    待批班级

---
+++ vue-element-admin/src/api/readPapers.js
@@ -0,0 +1,17 @@
+import request from '@/utils/request'
+
+// 获取已经分配教室的班级,得到待批班级的信息
+export function classInfo() {
+  return request({
+    url: '/manger/grade',
+    method: 'get'
+  })
+}
+// 获取学生试卷列表
+export function getStudent(data) {
+  return request({
+    url: '/exam/student',
+    method: 'get',
+    data
+  })
+}
